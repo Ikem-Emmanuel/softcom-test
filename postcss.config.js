eae662d83ca6d049924d5c2acdda2dab1a891e79
Ikem_Emmanuel <ikem43@gmail.com>
2020-08-31 10:26:11 +0100

    Css Purged

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,15 @@
 const tailwindcss = require('tailwindcss');
 module.exports = {
-	plugins: [tailwindcss('./tailwind.js'), require('autoprefixer'), require('@fullhuman/postcss-purgecss')],
+	plugins: [
+		tailwindcss('./tailwind.config.js'),
+		require('autoprefixer'),
+		require('@fullhuman/postcss-purgecss')({
+			content: [
+				'./src/App.js',
+				'./src/index.js',
+				'./public/index.html',
+			],
+			defaultExtractor: content => content.match(/[A-Za-z0-9-_:/{}``$]+/g) || []
+		}),
+	],
 };
